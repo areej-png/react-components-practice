a3274d7d77552c1dbeb7a2d8773554c752ee7a93
areej-png <areejamir890@gmail.com>
2025-10-20 23:50:42 -0700

    feat: add Hero section as Content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,15 @@
 function Content() {
   return (
-    <main>
-      <p>This is the main content of the website.</p>
-    </main>
+     <section className="hero" id="hero">
+      <div className="hero-text">
+        <h1>Find Your Pawfect Companion</h1>
+        <p>
+          Various resources and tools designed to help individuals find the right pet for their lifestyle,
+          including interactive tools, pet-sitting services, and training guides.
+        </p>
+        <a href="#viewpets" className="btn">View Pets</a>
+      </div>
+    </section>
   );
 }
 export default Content;
