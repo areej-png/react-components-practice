1269533b3200507cd04470293a93e83cf7ac30fa
areej-png <areejamir890@gmail.com>
2025-10-20 23:53:15 -0700

    feat: import Content component and Content CSS in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
+import Content from "./components/Content";
 import './Header.css';
+import './content.css';
 
 
 function App() {
   return (
     <>
       <Header />
-      {/* baaki sections yahan aayenge */}
+      <Content />
     </>
   );
 }
