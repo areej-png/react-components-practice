20a1617790bbd2e0f8ebe8c82e1091e6e57a3c96
areej-png <areejamir890@gmail.com>
2025-10-21 22:56:41 -0700

    feat: add Contact component and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import About from "./components/About";
 import React from "react";
-import ViewPets from "./components/viewpets" 
+import ViewPets from "./components/viewpets"
+import Contact from "./components/Contact"; 
 import './About.css';
 import './Header.css';
 import './content.css';
 import './pets.css';
+import './App.css'
 
 
 function App() {
@@ -18,6 +20,7 @@ function App() {
       <div className="App">
         <ViewPets />
       </div>
+      <Contact />
     </>
   );
 }
