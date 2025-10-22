a92b7885688b65fcd3053b0cd9d10468fb68d7ee
areej-png <areejamir890@gmail.com>
2025-10-21 21:34:03 -0700

    chore: set up src/data directory and add pets data file

---
+++ src/data/pets.js
@@ -0,0 +1,23 @@
+export const pets = [
+    {
+        id: 1,
+        name: "Puppy",
+        age: "Around 7 weeks old",
+        breed: "Golden Retriever",
+        image: "/puppy.png",
+    },
+    {
+        id: 2,
+        name: "Corillon",
+        age: "Few months old",
+        breed: "Papillon Mix",
+        image: "/Corillon.png",
+    },
+    {
+        id: 3,
+        name: "Pomeranian",
+        age: "Around 9 weeks old",
+        breed: "Playful temperament",
+        image: "/Pomeranian.png",
+    }
+];
