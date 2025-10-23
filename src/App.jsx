12870cf49bbd06d654c7ea318c112d9f7b066f32
areej-png <areejamir890@gmail.com>
2025-10-22 21:24:30 -0700

    feat: set dynamic page title in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import About from "./components/About";
-import React from "react";
-import ViewPets from "./components/viewpets"
+import ViewPets from "./components/viewpets";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-import './About.css';
-import './Header.css';
-import './content.css';
-import './pets.css';
-import './App.css';
-import './footer.css'
-
+import "./About.css";
+import "./Header.css";
+import "./content.css";
+import "./pets.css";
+import "./App.css";
+import "./footer.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "Pawfect Shelter";
+  }, []);
+
   return (
     <>
       <Header />
       <main>
-      <Content />
-      <About />
-      <div className="App">
-        <ViewPets />
-      </div>
-      <Contact />
+        <Content />
+        <About />
+        <div className="App">
+          <ViewPets />
+        </div>
+        <Contact />
       </main>
       <Footer />
     </>
